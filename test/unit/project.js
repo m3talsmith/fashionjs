191c4145fdd30d595aaaa6eec8b5ce89158faede
Michael Christenson II <Michael Christenson II>
2014-01-25 16:15:20 -0700

    cleaned up for new tests

--- test/unit/project.js
+++ test/unit/project.js
@@ -1,71 +1,22 @@
 var chai = require('chai'),
     path = require('path'),
     fs   = require('fs'),
-    Git  = require('git-wrapper2'),
     exec = require('child_process').exec;
 
 var assert = chai.assert,
     expect = chai.expect,
     should = chai.should;
 
-function repoPath(name) { return path.join(__dirname, '../dummy', name); }
-
-function createRepo (name, options, callback) {
-  var gitPath = path.join(repoPath(name), '.git');
-  var git = new Git({'git-dir': gitPath});
-  fs.exists(gitPath, function (exists) {
-    if(exists) {
-      destroyTestRepo(name, function (error, stdout, stderr) {
-        initRepo(git, options, callback);
-      });
-    } else {
-      initRepo(git, options, callback);
-    }
-  });
-}
-
-function initRepo (git, options, callback) {
-  var initString = options.bare ? 'init --bare' : 'init';
-  git.exec(initString, function (error, message) {
-    callback(git, error, message);
-  });
-}
-
-function destroyTestRepo (name, callback) {
-  exec('rm -rf ' + path.join(repoPath(name), '.git'), callback);
-}
-
 describe('Project', function () {
-  beforeEach(function () {
-    createRepo('upstream', {bare: true}, function (upstream, error, message) {
-       createRepo('deployedOld', {}, function (old, error, message) {
-         old.exec('remote add upstream ' + repoPath('upstream'), function (error, message) {
+  beforeEach(function () {});
 
-         });
-       });
-    });
-  });
-
-  describe('#deploy()', function () {
-    it('gets diff between upstream and deployed code', function () {
-      var git = new Git({'git-dir': path.join(repoPath('deployedOld'), '.git')});
-
-      git.exec('diff upstream master', function (error, message) {
-        expect(message).to.equal(true);
-      });
-    });
-
-    it('has current code deployed');
-    it('has older code code deployed');
-    it('fails deploy with bad repo');
-    it('deploys via git');
-    it('sets latest deploy timestamp');
-  });
+  describe('#config()', function () {});
+  describe('#deploy()');
   describe('#report()', function () {
     it('shows a deploy is needed');
     it('shows a deploy is not needed');
     it('shows the last commit info');
     it('shows the last deploy timestamp');
   });
-  describe('#rollback()', function () {});
+  describe('#rollback()');
 });
